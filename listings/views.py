3b063a973fe0256b6c2269f2fc25ad5f05866cb0
garisonmike <garisonmike006@gmail.com>
2025-09-08 11:19:31 +0300

    Clean up old listings/ and root settings.py after restructure

--- listings/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import JsonResponse
-
-def home(request):
-    """
-    A simple placeholder view for the Listings app.
-    This will be expanded in future milestones.
-    """
-    return JsonResponse({"message": "Welcome to ALX Travel Listings!"})
